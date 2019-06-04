93deb86adf59b352b605081541db4fa06c943a9e
thebhatman <manjunathbhat9920@gmail.com>
2019-06-04 20:32:07 +0530

    Correcting the transformation.

--- vision/mnist/stn.jl
+++ vision/mnist/stn.jl
@@ -26,6 +26,7 @@ function affine_grid_generator(height, width, theta)
 	sampling_grid = Array(reshape(transpose(repeat(transpose(sampling_grid), batch_size)), 3, size(x_t_flat)[2], batch_size))
 
 	batch_grids = batched_gemm('N', 'N', theta, sampling_grid)
+
 	y_s = permutedims(reshape(batch_grids[2, :, :], width, height, batch_size), [2,1,3])
 	x_s = permutedims(reshape(batch_grids[1, :, :], width, height, batch_size), [2,1,3])
 	return x_s, y_s
@@ -37,15 +38,25 @@ function get_pixel_values(img, x, y)
 	height = size(img, 2)
 	channels = size(img, 3)
 	#println(y)
+
 	x_indices = trunc.(Int, Array(selectdim(x, 1, 1)))
 	y_indices = trunc.(Int, Array(selectdim(y, 2, 1)))
+
+	println("*************************************")
+	println((x_indices[:, 1]))
+	println("//////////////////////////////////////")
+	println((y_indices[:, 1]))
+
 	batch = []
 	#println(size(img))
 	println(batch_size)
 	for i in 1:batch_size
-		push!(batch, img[y_indices[:, i], x_indices[:, i], :, i])
+		#push!(batch, img[y_indices[:, i], x_indices[:, i], :, i])
+		pic = colorview(RGB, reshape(img[:, :, :, i], channels, width, height))
+		new_pic = pic[y_indices[:, i], x_indices[:]]
+		push!(batch, reshape(Float64.(channelview(new_pic)), width, height, channels))
 	end
-	batch = reshape(hcat(batch...),width, height, channels, batch_size)
+	batch = reshape(cat(batch..., dims = 4),width, height, channels, batch_size)
 	return batch
 end
 
@@ -78,6 +89,16 @@ function bilinear_sampler(img, x, y)
 	valB = get_pixel_values(img, x0, y1)
 	valC = get_pixel_values(img, x1, y0)
 	valD = get_pixel_values(img, x1, y1)
+
+	new_img = colorview(RGB, reshape(valA[:, :, :, 1], 3, 64, 64))
+	imshow(new_img)
+	new_img = colorview(RGB, reshape(valB[:, :, :, 1], 3, 64, 64))
+	imshow(new_img)
+	new_img = colorview(RGB, reshape(valC[:, :, :, 1], 3, 64, 64))
+	imshow(new_img)
+	new_img = colorview(RGB, reshape(valD[:, :, :, 1], 3, 64, 64))
+	imshow(new_img)
+
 	weight1 = []
 	weight2 = []
 	weight3 = []
@@ -88,20 +109,27 @@ function bilinear_sampler(img, x, y)
 		push!(weight3, w3)
 		push!(weight4, w4)
 	end
-	weight1 = permutedims(reshape(hcat(weight1...), height, width, batch_size, channels), [1, 2, 4, 3])
-	weight2 = permutedims(reshape(hcat(weight2...), height, width, batch_size, channels), [1, 2, 4, 3])
-	weight3 = permutedims(reshape(hcat(weight3...), height, width, batch_size, channels), [1, 2, 4, 3])
-	weight4 = permutedims(reshape(hcat(weight4...), height, width, batch_size, channels), [1, 2, 4, 3])
 
-	return weight1 .* valA + weight2 .* valB + weight3 .* valC + weight4 .* valD
+	weight1 = permutedims(reshape(cat(weight1..., dims = 4), height, width, batch_size, channels), [1, 2, 4, 3])
+	weight2 = permutedims(reshape(cat(weight2..., dims = 4), height, width, batch_size, channels), [1, 2, 4, 3])
+	weight3 = permutedims(reshape(cat(weight3..., dims = 4), height, width, batch_size, channels), [1, 2, 4, 3])
+	weight4 = permutedims(reshape(cat(weight4..., dims = 4), height, width, batch_size, channels), [1, 2, 4, 3])
+
+	resultant = weight1 .* valA + weight2 .* valB + weight3 .* valC + weight4 .* valD
+	return resultant
 end
 
 theta = Matrix{Float64}(I, 2, 3)
+#theta[2, 3] = 0.5
+# theta[1, 1] = cos(pi/6)
+# theta[1, 2] = -sin(pi/6)
+# theta[2, 1] = sin(pi/6)
+# theta[2, 2] = cos(pi/6)
 theta = Array(reshape(transpose(repeat(transpose(theta), 1)), 2, 3, 1))
-println("Verifying : ")
-println(affine_grid_generator(32, 16, theta)[1])
-println("-----------------------------------------------")
-println(affine_grid_generator(32, 16, theta)[2])
+# println("Verifying : ")
+# println(affine_grid_generator(32, 16, theta)[1])
+# println("-----------------------------------------------")
+# println(affine_grid_generator(32, 16, theta)[2])
 
 #Predicts the 6 parameters of transformation matrix for each image in the batch.
 localization_net = Chain(MaxPool((2, 2)), Conv((5, 5), 1 => 20, stride = (1, 1), pad = (0, 0)),
